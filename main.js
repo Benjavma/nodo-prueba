57b94c6743270515d99ec973856d256f877e9559
Benjavma <40669110+Benjavma@users.noreply.github.com>
2018-12-23 16:05:01 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueRouter from 'vue-router'
+import routes from './routes'
+
+/*
+	vue-router installation and configuration
+*/
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
+
+Vue.use(VueRouter)
+
+/*
+  vue installation.
+*/
+
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router
+})
+
+export {router}
